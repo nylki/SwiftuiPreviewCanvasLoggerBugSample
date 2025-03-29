6575e856e27b025d770eed5f52530d3de30f5ad8
Tom Brewe <contact@tombrewe.de>
2025-03-29 16:39:38 +0100

    add sample project that reproduces the issue

---
+++ SwiftuiPreviewCanvasLoggerBugSample/SwiftuiPreviewCanvasLoggerBugSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiPreviewCanvasLoggerBugSampleApp.swift
+//  SwiftuiPreviewCanvasLoggerBugSample
+//
+//  Created by Tom Brewe on 29.03.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiPreviewCanvasLoggerBugSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
